01e55e7dfa552975030a415a2265631425277106
Limin Zhang <zhang.lim@northeastern.edu>
2023-02-15 20:44:43 -0500

    z

--- hw/hw2/Part2_2/scripts.js
+++ hw/hw2/Part2_2/scripts.js
@@ -15,13 +15,9 @@ d3.csv("new.csv").then(function(data) {
     let filtered_data_sex_1900 = data.filter(function(d) {
         return d.Sex === '1' && d.Year === '1900';
     });
+
     console.log(filtered_data_sex_1900);
     
-    // let pop1900Num = data.(function(d){
-    //     return d.People;
-    // });
-    // console.log(pop1900Num);
-
     let filtered_data_sex_2000 = data.filter(function(d) {
         return d.Sex === '1' && d.Year === '2000';  
     });
@@ -31,7 +27,7 @@ d3.csv("new.csv").then(function(data) {
         min: d3.min(filtered_data_sex_1900, function(d) { return +d.People; }),
         max: d3.max(filtered_data_sex_1900, function(d) { return +d.People; }),
     };
-    console.log(pop1900);
+    //console.log(pop1900);
     
     const pop2000 = {
         min: d3.min(filtered_data_sex_2000, function(d) { return +d.People; }),
@@ -63,24 +59,24 @@ d3.csv("new.csv").then(function(data) {
         .call(d3.axisLeft().scale(yScale));
 
     /* DRAW BARS*/
-    // const points1900 = svg.selectAll("rect")
-    //     .data(filtered_data_sex_1900)                                                                        /* Joins data to the selected elements which is all the rectangles in this case */
-    //     .enter()                                                                                    /* Creates a selection with placeholder references for missing elements */
-    //     .append("rect")                                                                             /* Create rectangles in each place holder */
-    //     .attr("x", function(d) { return xScale(d.year); })
-    //     .attr("y", function(d) { return yScale(filtered_data_sex_1900, function(d) { return +d.People; }); })
-    //     .attr("width", xScale.bandwidth())
-    //     .attr("height", function(d) { return height - margin.bottom - yScale(filtered_data_sex_1900, function(d) { return +d.People; }); })
-    //     .attr("fill", "orange");
+    const points1900 = svg.selectAll("rect")
+        .data(filtered_data_sex_1900)                                                                        /* Joins data to the selected elements which is all the rectangles in this case */
+        .enter()                                                                                    /* Creates a selection with placeholder references for missing elements */
+        .append("rect")                                                                             /* Create rectangles in each place holder */
+        .attr("x", function(d) { return xScale(d.Year); })
+        .attr("y", function(d) { return yScale(+d.People) }); })
+        .attr("width", xScale.bandwidth())
+        .attr("height", function(d) { return height - margin.bottom - yScale(+d.People); })
+        .attr("fill", "orange");
 
     // const points2000 = svg.selectAll("rect")
     //     .data(filtered_data_sex_2000)                                                                        /* Joins data to the selected elements which is all the rectangles in this case */
     //     .enter()                                                                                    /* Creates a selection with placeholder references for missing elements */
     //     .append("rect")                                                                             /* Create rectangles in each place holder */
-    //     .attr("x", function(d) { return xScale(d.year); })
-    //     .attr("y", function(d) { return yScale(d.pop2000); })
+    //     .attr("x", function(d) { return xScale(d.Year); })
+    //     .attr("y", function(d) { return yScale(d.People); })
     //     .attr("width", xScale.bandwidth())
-    //     .attr("height", function(d) { return height - margin.bottom - yScale(d.pop2000); })
+    //     .attr("height", function(d) { return height - margin.bottom - yScale(d.People); })
     //     .attr("fill", "steelblue");
     
     /*DRAW AXIS LABELS*/
@@ -96,11 +92,7 @@ d3.csv("new.csv").then(function(data) {
         .attr("x",-height/2)
         .attr("y",margin.left/2)
         .text("Population");
-
-        //tooltip
-       // const tooltip = d3.selet(#chart)
-
-});
+}
 
 
 
@@ -133,90 +125,3 @@ d3.csv("new.csv").then(function(data) {
 
 
 
-
-
-
-
-
-// d3.csv("new.csv")
-//     .then(function(data) { 
-
-        // /* DEFINE DIMENSIONS OF SVG + CREATE SVG CANVAS*/
-
-        // const width = document.querySelector("#chart").clientWidth;
-        // const height = document.querySelector("#chart").clientHeight;
-        // const margin = {top: 50, left: 100, right: 50, bottom: 100};
-
-        // const svg = d3.select("#chart")
-        //     .append("svg")
-        //     .attr("width", width)
-        //     .attr("height", height);
-
-        // /* DETERMINE MIN AND MAX VALUES OF VARIABLES */
-        // const malePop1900 = {
-        //     min: d3.min(d.Male1900),
-        //     max: d3.max(d.Male1900)
-        // };
-
-        // const malePop2000 = {
-        //     min: d3.min(d.Male2000),
-        //     max: d3.max(d.Male2000)
-        // };
-
-        // /*CREATE SCALES */
-        // const xScale = d3.scaleBand()
-        //     .domain(["0","5","10","15","20","25","30","30","35","40","45","50","55","60","65","70","75","80","85","90"])
-        //     .range([margin.left, width-margin.right])
-        //     .padding(0.5);
-
-        // const yScale = d3.scaleLinear()
-        //     .domain([0, malePop2000.max])
-        //     .range([height-margin.bottom, margin.top]);
-       
-        // /* DRAW AXES*/
-        // const xAxis = svg.append("g")
-        //     .attr("class","axis")
-        //     .attr("transform", `translate(0,${height-margin.bottom})`)
-        //     .call(d3.axisBottom().scale(xScale));
-
-        // const yAxis = svg.append("g")
-        //     .attr("class","axis")
-        //     .attr("transform", `translate(${margin.left},0)`)
-        //     .call(d3.axisLeft().scale(yScale));
-
-        // /* DRAW*/
-        // const points1 = svg.selectAll("rect")
-        //     .data(filtered_data)                                                                        /* Joins data to the selected elements which is all the rectangles in this case */
-        //     .enter()                                                                                    /* Creates a selection with placeholder references for missing elements */
-        //     .append("rect")                                                                             /* Create rectangles in each place holder */
-        //     .attr("x", function(d) { return xScale(d.Age); })
-        //     .attr("y", function(d) { return yScale(d.Male1900); })
-        //     .attr("width", xScale.bandwidth())
-        //     .attr("height", function(d) { return height - margin.bottom - yScale(d.lifeExp); })
-        //     .attr("fill", "steelblue");
-
-        // // const points2 = svg.selectAll("rect")
-        // //     .data(filtered_data)                                                                        /* Joins data to the selected elements which is all the rectangles in this case */
-        // //     .enter()                                                                                    /* Creates a selection with placeholder references for missing elements */
-        // //     .append("rect")                                                                             /* Create rectangles in each place holder */
-        // //     .attr("x", function(d) { return xScale(d.Age); })
-        // //     .attr("y", function(d) { return yScale(d.Male2000); })
-        // //     .attr("width", xScale.bandwidth())
-        // //     .attr("height", function(d) { return height - margin.bottom - yScale(d.lifeExp); })
-        // //     .attr("fill", "yellow");
-
-        // /* DRAW AXIS LABELS*/
-        // const xAxisLabel = svg.append("text")
-        //     .attr("class","axisLabel")
-        //     .attr("x", width/2)
-        //     .attr("y", height-margin.bottom/2)
-        //     .text("Age");
-
-        // const yAxisLabel = svg.append("text")
-        //     .attr("class","axisLabel")
-        //     .attr("transform","rotate(-90)")
-        //     .attr("x",-height/2)
-        //     .attr("y",margin.left/2)
-        //     .text("Male Population");
-
-    // });
