fb1800398947e56d4c24ab231bdd81d6f4598e90
Limin Zhang <zhang.lim@northeastern.edu>
2023-03-21 14:55:44 -0400

    v

--- hw/hw5/scripts/tab-scripts.js
+++ hw/hw5/scripts/tab-scripts.js
@@ -1,20 +1,20 @@
-function sbLevel(event, sbLevel) {
+function sbLevel(event, level) {
     // Declare all variables
     var i, tab2content, tab2links;
   
-    // Get all elements with class="tabcontent" and hide them
+    // Get all elements with class="tab2content" and hide them
     tab2content = document.getElementsByClassName("tab2content");
     for (i = 0; i < tab2content.length; i++) {
       tab2content[i].style.display = "none";
     }
   
-    // Get all elements with class="tablinks" and remove the class "active"
+    // Get all elements with class="tab2links" and remove the class "active"
     tab2links = document.getElementsByClassName("tab2links");
     for (i = 0; i < tab2links.length; i++) {
       tab2links[i].className = tab2links[i].className.replace(" active", "");
     }
   
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(level).style.display = "block";
     evt.currentTarget.className += " active";
   }
