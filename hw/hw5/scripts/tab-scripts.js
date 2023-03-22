e12ae245fd2eebd7ae10d2ea8ba1fb570edfc15b
Limin Zhang <zhang.lim@northeastern.edu>
2023-03-21 20:19:41 -0400

    d

--- hw/hw5/scripts/tab-scripts.js
+++ hw/hw5/scripts/tab-scripts.js
@@ -1,24 +1,24 @@
-// tab 1
-function sbEssential(event, essential) {
-    // Declare all variables
-    var i, tab1content, tab1links;
+// // tab 1
+// function sbEssential(event, essential) {
+//     // Declare all variables
+//     var i, tab1content, tab1links;
   
-    // Get all elements with class="tab2content" and hide them
-    tab1content = document.getElementsByClassName("tab1content");
-    for (i = 0; i < tab1content.length; i++) {
-      tab2content[i].style.display = "none";
-    }
+//     // Get all elements with class="tab2content" and hide them
+//     tab1content = document.getElementsByClassName("tab1content");
+//     for (i = 0; i < tab1content.length; i++) {
+//       tab2content[i].style.display = "none";
+//     }
   
-    // Get all elements with class="tab2links" and remove the class "active"
-    tab1links = document.getElementsByClassName("ta1links");
-    for (i = 0; i < tab1links.length; i++) {
-      tab1links[i].className = tab1links[i].className.replace(" active", "");
-    }
+//     // Get all elements with class="tab2links" and remove the class "active"
+//     tab1links = document.getElementsByClassName("ta1links");
+//     for (i = 0; i < tab1links.length; i++) {
+//       tab1links[i].className = tab1links[i].className.replace(" active", "");
+//     }
   
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(level).style.display = "block";
-    evt.currentTarget.className += " active";
-  };
+//     // Show the current tab, and add an "active" class to the button that opened the tab
+//     document.getElementById(level).style.display = "block";
+//     evt.currentTarget.className += " active";
+//   };
 
 // tab2   
 function sbLevel(event, level) {
